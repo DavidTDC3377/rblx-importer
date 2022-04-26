c7a90370f54e768c1bdb52a4d075d15ee2f87adf
David <40366903+DavidTDC3377@users.noreply.github.com>
2022-04-26 10:00:47 +0100

    Created the module
    The module .lua file was created and release will be dispatched soon.

---
+++ RBLX_IMPORTER.lua
@@ -0,0 +1,8 @@
+-- github/DavidTDC3377/rblx-importer
+
+local importer-sys = {}
+local scripts = {
+  policy-service_module = "https://raw.githubusercontent.com/DavidStudios/policy-service/main/policy-module.lua", 
+  policy-service_gettr = nil -- in works
+ }
+
